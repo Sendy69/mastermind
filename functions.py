9506e9ae697520ba7d0192206b3e7c56b07b9e51
Sandrine Nokam <sandrinewato27@gmail.com>
2024-06-24 03:54:53 +0200

    management of the function player_initialize_combination, it's working well git status

--- functions.py
+++ functions.py
@@ -1,78 +1,84 @@
 ############### THIS FILE CONTAINS ALL FUNCTIONS OF THE MASTERMIND  #############
 import random
 
-# constants Initialization 
-SET_OF_NUMBERS = {1, 2, 3, 4, 5, 6}
+# constants Initialization
+### Initialization of set combination 
+SET_OF_NUMBERS = set(random.randint(1, 10) for _ in range(6))
 
 
-## This functions allows to initialize game by generate randomly a combinaison of 04 numbers 
-def generate_combinaison():
+## This functions allows to initialize game by generate randomly a combination of 04 numbers 
+def generate_combination():
     
-    initial_combinaison = [random.choice(list(SET_OF_NUMBERS)) for _ in range (4)]
-    print(initial_combinaison)
+    initial_combination = [random.choice(list(SET_OF_NUMBERS)) for _ in range (4)]
+    print(initial_combination)
+    return initial_combination
 
-    return initial_combinaison
-
-# This function allows to initialize players combinaison
-def player_initialize_combinaison():
-    
-    player_combinaison = []
-    for i in range (0,4):
-        i = i + 1
-        combinaison = int(input(f"Entrée votre combinaison N°{i} \n"))
-        if combinaison in SET_OF_NUMBERS:
-            player_combinaison.append(combinaison)
-        else:
+# This function allows to initialize players combination
+def player_initialize_combination():    
+    player_combination = []
+    i = 1
+    while i < 5:
+        combination = int(input(f"Entrée votre combinaison N°{i} \n"))
+        if combination in SET_OF_NUMBERS:
+            player_combination.append(combination)
+            i += 1 
+        else :
             print("la combinaison choisie n'existe pas dans le champ de possibilité ci-dessus:")
             print(f"{SET_OF_NUMBERS}")
-            print("Réssayer à nouveau !!")
+            print("Réssayer à nouveau !!\n")
+            combination = int(input(f"Entrée votre combinaison N°{i} \n"))
+            player_combination.append(combination)
+            i += 1 
 
-            ### IL y'a un problème à ce niveau
-            player_initialize_combinaison()
-    
     print("*********************************************")
-    print(f"votre combinaison est {player_combinaison}  **")
+    print(f"votre combination est {player_combination}  **")
     print("*********************************************")
 
-    return player_combinaison
+    return player_combination
 
-# This function allows to check the existence of the combinaison of the player and the initial combinaison and return which element is correct,
+# This function allows to check the existence of the combination of the player and the initial combination and return which element is correct,
 # check also if it's at the correct position by using the symbol (*) or wrong position by using this symbol(°) 
 
-def check_combinaisons(player_combinaison, initial_combinaison):
-    check_combinaison = []
-    for position, number in enumerate(player_combinaison):
+def check_combinations(player_combination, initial_combination):
+    check_combination = []
+    count_of_number = 0
+    for position, number in enumerate(player_combination):
       
-        if number in initial_combinaison:
-            ### IL y'a un problème à ce niveau
-            if number == initial_combinaison[position]:
-                check_combinaison.append(f"{number}*")
-                
-            else:
-                check_combinaison.append(f"{number}°")
-                
-        else:
-            check_combinaison.append(f"{number}")
-    return check_combinaison
+        if number in initial_combination:
+            ####### PROBLEME RENCONTRE: Gestion des positionnements 
 
-def turn_manager(initial_combinaison):
+            ### To count the occurence of a number in the initial list
+            count_of_number = initial_combination.count(number)
+            while count_of_number > 0: 
+                if number == initial_combination[position]:
+                    check_combination.append(f"{number}*")
+                    count_of_number -=1
+                else:
+                    check_combination.append(f"{number}°")
+                    count_of_number -=1          
+        else:
+            check_combination.append(f"{number}")
+    return check_combination
+
+# This function allows to manage player's turns game which is 10 turns
+def turn_manager(initial_combination):
     print("Vous avez droit à 10 tentatives\n")
     numbers_of_turns = 0
     while numbers_of_turns < 11:
         print(f"Vous êtes au tour N° {numbers_of_turns + 1} sur 10\n")
 
-        player_combinaison = player_initialize_combinaison()
-        check_combinaison = check_combinaisons(player_combinaison, initial_combinaison)
-        correct_combinaison = 0
+        player_combination = player_initialize_combination()
+        check_combination = check_combinations(player_combination, initial_combination)
+        correct_combination = 0
 
-        print(check_combinaison)
-        for combinaison in check_combinaison:                    
-            if "*" in combinaison:
-                correct_combinaison +=1
-        if correct_combinaison == 4:
+        print(check_combination)
+        for combination in check_combination:                    
+            if "*" in combination:
+                correct_combination +=1
+        if correct_combination == 4:
             print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
             print("°                    Vous avez trouvez la combinaison exacte !!                                                         °")
-            print(f"°                          {initial_combinaison}                                                              °")
+            print(f"°                          {initial_combination}                                                              °")
             print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
             break
 
@@ -80,11 +86,13 @@ def turn_manager(initial_combinaison):
         if numbers_of_turns == 10:
             print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°° Vous avez épuisé vos essais °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n")
             print("                                     Vous n'avez pas trouvez la combinaison correct                                    °°")
-            print(f"                                 la combinaison correcte est : {initial_combinaison}                                  °°")
+            print(f"                                 la combinaison correcte est : {initial_combination}                                  °°")
             print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
             break
 
+### Faire une fonction pour l'affichage des règles du jeu
 
-initial_combinaison = generate_combinaison()
-turn_manager(initial_combinaison)
+
+initial_combination = generate_combination()
+turn_manager(initial_combination)
 
